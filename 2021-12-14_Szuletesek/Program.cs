3b11d48ae3ae5ad6f424e438cf00631617f7031f
Soldier46 <rschuszler46@gmail.com>
2021-12-14 10:18:31 +0100

    Add project files.

---
+++ 2021-12-14_Szuletesek/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2021_12_14_Szuletesek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
